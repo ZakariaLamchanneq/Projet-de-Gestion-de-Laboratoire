4f456e5c085ba06c544f8bc439109e169c70c83a
Zakaria <zakarialam19@gmail.com>
2024-10-14 22:13:14 +0100

    Initialisation de la structure du projet avec les répertoires backend et frontend

---
+++ Back-End/src/test/java/Back_End/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Back_End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
