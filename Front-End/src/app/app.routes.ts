3c0596037838ddeea43ba71ce686698dc13746f0
Zakaria <zakarialam19@gmail.com>
2024-11-20 13:52:28 +0100

    Patient CRUD et son interface

--- Front-End/src/app/app.routes.ts
+++ Front-End/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LaboratoireFormComponent } from './components/laboratoire/laboratoire-form/laboratoire-form.component';
 import {UtilisateurComponent} from './components/utilisateur/utilisateur.component';
 import { LaboratoireComponent } from './components/laboratoire/laboratoire.component';
+import {PatientComponent} from './components/patient/patient.component';
 
 export const routes: Routes = [
   // Redirect the root to /home
@@ -10,6 +11,7 @@ export const routes: Routes = [
   // Optionally add a wildcard route for unknown paths
   { path: 'laboratoires',  component: LaboratoireComponent },
   { path: 'utilisateurs',  component: UtilisateurComponent },
+  { path: 'patients', component: PatientComponent},
   { path: '**', redirectTo: '' },
 ];
 
