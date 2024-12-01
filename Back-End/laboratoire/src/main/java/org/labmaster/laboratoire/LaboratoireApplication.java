f0e959d6cae2b634ba47d28bc0f6e1a76f114a5e
Zakaria <zakarialam19@gmail.com>
2024-12-01 12:02:52 +0100

    Configuration Spring Security (authentification-autorisations)/ Liaison avec page Login

--- Back-End/laboratoire/src/main/java/org/labmaster/laboratoire/LaboratoireApplication.java
+++ Back-End/laboratoire/src/main/java/org/labmaster/laboratoire/LaboratoireApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class LaboratoireApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LaboratoireApplication.class, args);
 	}
 
--- Front-End/src/app/components/page-login/page-login.component.ts
+++ Front-End/src/app/components/page-login/page-login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
-import {Router} from '@angular/router';
-import {LayoutComponent} from '../navigation/layout/layout.component';
-import {NzIconDirective} from 'ng-zorro-antd/icon';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LayoutComponent } from '../navigation/layout/layout.component';
+import { NzIconDirective } from 'ng-zorro-antd/icon';
+import { AuthService } from '../../services/AuthService/auth-service.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-page-login',
@@ -10,18 +12,43 @@ import {NzIconDirective} from 'ng-zorro-antd/icon';
   imports: [
     ReactiveFormsModule,
     LayoutComponent,
-    NzIconDirective
+    NzIconDirective,
+    NgIf
   ],
   templateUrl: './page-login.component.html',
-  styleUrl: './page-login.component.css'
+  styleUrls: ['./page-login.component.css'] // Corrected property name to styleUrls
 })
 export class PageLoginComponent {
+  loginForm: FormGroup;
+  errorMessage = '';
 
-  constructor(
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) {
 
-  login(){
-   this.router.navigate(["/utilisateurs"])
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      return; // Prevent submission if the form is invalid
+    }
+
+    const { email, password } = this.loginForm.value;
+    console.log(this.loginForm.value);
+
+    this.authService.login({ email, password }).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/patients']);
+      },
+      error: (err) => {
+        console.error('Login error:', err);
+        this.errorMessage = 'Invalid email or password';
+      }
+    });
+
   }
 }
