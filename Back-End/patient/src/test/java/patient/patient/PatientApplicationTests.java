2c16d6934f033606ecbd8986ab6919e64d8096c2
Oussama El Akhiri <oussama.elakhiri@usms.ac.ma>
2024-12-29 17:44:00 +0100

    testing pipeline v2

--- Back-End/patient/src/test/java/patient/patient/PatientApplicationTests.java
+++ Back-End/patient/src/test/java/patient/patient/PatientApplicationTests.java
@@ -2,8 +2,10 @@ package patient.patient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PatientApplicationTests {
 
 	@Test
